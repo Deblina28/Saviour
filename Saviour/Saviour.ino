1278dc9f9646fcac43ba60177adb7d1b0cd096a3
Deblina Chattopadhyay <62389473+Deblina28@users.noreply.github.com>
2021-07-18 12:59:00 +0530

    Added the GPS

--- Saviour/Saviour.ino
+++ Saviour/Saviour.ino
@@ -1,8 +1,18 @@
 #include <avr/pgmspace.h>
 #include <Arduino.h>
 #include <U8x8lib.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+#define led 7
+#define buzz 5
 
 U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(SCL, SDA, U8X8_PIN_NONE);   // OLEDs without Reset of the Display
+
+
+SoftwareSerial ss(4, 3);
+TinyGPSPlus gps;
+
 int fran = 233;
 
 int dit = 80;
@@ -11,67 +21,139 @@ int dah = 200;
 
 char l = ' ';
 String sent = "";
-int x = 0, y = 0;
-boolean f = false, cursor = false, goMenu = false;
+int x = 0, y = 0, ch=0, lch=-1;
+boolean f = false, cursor = false, goMenu = true;
 long ps = 0, out;
 
 static const String morse[] PROGMEM = {"-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "---...", "---...", "-...-", "-...-", "-...-", "..--..", ".--.-.", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
 
+String lati="",lngt="";
+
+
 void setup() {
   pinMode(2, INPUT);
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(buzz, OUTPUT);
+  pinMode(led, OUTPUT);
 
   u8x8.begin();
   u8x8.setFlipMode(0);
   u8x8.setBusClock(100000);
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-
-  Serial.begin(9600);
-
-
-
+  ss.begin(9600);
+  Serial.begin(115200);
 }
-
-
 
 
 
 void loop()
 {
 
-  if (!goMenu)
+  while (!goMenu)
   {
+      if(ch==0)
+        Morse();
+        
+      else if(ch==2)
+      GPS();
+     
+  }
+      
+    
+    
 
-    l = (char)(90 - map(analogRead(A0), 0, 1021, 0, 26));
+  selectMenu();
+
+    
+  
+}
+
+
+
+
+void selectMenu()
+{
+  ch=map(analogRead(A0), 0,1020,0,3);
+  Serial.println(ch);
+  x=0;
+  y=0;
+  
+if(lch!=ch)
+{
+  if(ch==0)
+  {
+   u8x8.clearDisplay();
+   u8x8.setCursor(4,3);
+   u8x8.print("Morse");
+   u8x8.setCursor(4,4);
+   u8x8.print("Code");
+   u8x8.setCursor(3,6);
+   u8x8.print("-.-.-.-");
+  }
+
+  else if(ch==1)
+  {
+   u8x8.clearDisplay();
+   u8x8.setCursor(4,3);
+   u8x8.print("Compass");
+  }
+
+  else if(ch==2)
+  {
+   u8x8.clearDisplay();
+   u8x8.setCursor(4,3);
+   u8x8.print("GPS");
+  }
+
+  else if(ch==3)
+  {
+   u8x8.clearDisplay();
+   u8x8.setCursor(4,3);
+   u8x8.print("Altitude");
+  }
+  
+  lch=ch;
+}
+
+
+if(digitalRead(2)==1)
+{
+  u8x8.clearDisplay();
+  goMenu=false;
+  delay(100);  
+}
+
+
+out=millis();
+}
+
+
+
+
+boolean Morse()
+{
+  l = (char)(90 - map(analogRead(A0), 0, 1021, 0, 26));
 
     if (l == 64)
       l = ' ';
 
     u8x8.setCursor(x, y);
-    click();
-
-    if (millis() - ps >= 500)
-    {
-      cursor = !cursor;
-      ps = millis();
-    }
-
-    if (cursor)
-      u8x8.print(l);
-    else
-      u8x8.print('_');
-  }
-}
-
-
-boolean click()
-{
+  
   if (digitalRead(2))
   {
-
     if (f)
-      Morse();
+    {
+      sent += l;
+      x++;
+      f = false;
+      u8x8.print(l);
+      if (x == 16)
+      {
+        y++;
+        x = 0;
+        u8x8.setCursor(x, y);
+      }
+      Serial.println(sent);
+    }
 
     if (millis() - out > 3000)
     {
@@ -81,6 +163,15 @@ boolean click()
       printMorse(z);
       spitMorse(z);
       goMenu = true;
+      while(true)
+      {
+        if(digitalRead(2))
+        {
+          delay(100);
+          return !f;  
+        }          
+      }
+      
     }
   }
 
@@ -90,23 +181,20 @@ boolean click()
     out = millis();
   }
 
+  if (millis() - ps >= 500)
+    {
+      cursor = !cursor;
+      ps = millis();
+    }
+
+    if (cursor)
+      u8x8.print(l);
+    else
+      u8x8.print('_');
+
   return !f;
 }
 
-void Morse()
-{
-  sent += l;
-  x++;
-  f = false;
-  u8x8.print(l);
-  if (x == 16)
-  {
-    y++;
-    x = 0;
-    u8x8.setCursor(x, y);
-  }
-  Serial.println(sent);
-}
 
 String morseTranslate(String line)
 {
@@ -152,36 +240,106 @@ void spitMorse(String s)
 
     if (x == ' ')
     {
-      digitalWrite(4, 0);
-      analogWrite(5, 0);
+      digitalWrite(led, 0);
+      analogWrite(buzz, 0);
       delay(200);
     }
 
     else if (x == '.')
     {
-      analogWrite(5, fran);
-      digitalWrite(4, 1);
+      analogWrite(buzz, fran);
+      digitalWrite(led, 1);
       delay(dit);
-      analogWrite(5, 0);
-      digitalWrite(4, 0);
+      analogWrite(buzz, 0);
+      digitalWrite(led, 0);
     }
 
     else if (x == '-')
     {
-      analogWrite(5, fran);
-      digitalWrite(4, 1);
+      analogWrite(buzz, fran);
+      digitalWrite(led, 1);
       delay(dah);
-      analogWrite(5, 0);
-      digitalWrite(4, 0);
+      analogWrite(buzz, 0);
+      digitalWrite(led, 0);
     }
 
     else
     {
-      analogWrite(5, 0);
-      digitalWrite(4, 0);
+      analogWrite(buzz, 0);
+      digitalWrite(led, 0);
       delay(500);
     }
 
     delay(50);
   }
+}
+
+
+void GPS()
+{
+   while (ss.available() > 0)
+  {
+    if (gps.encode(ss.read()))
+      displayGpsData();
+
+      long cs=millis();
+      while(digitalRead(2))
+      {
+        Serial.println(millis()-cs);
+        if(millis()-cs>3000)
+        {
+          String li=morseTranslate(lati);
+          String lg=morseTranslate(lngt);
+          printMorse(li);
+          spitMorse(li);
+          printMorse(lg);
+          spitMorse(lg);
+
+          while(true)
+          {
+            if(digitalRead(2))
+            {
+              goMenu=true;
+              delay(100);
+              return;
+            }
+          }
+        }
+      }
+      
+  }
+}
+
+
+void displayGpsData()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    lati = "lat:"+String(gps.location.lat(), 6);
+    lngt = "lng:"+String(gps.location.lng(), 6);
+
+    u8x8.refreshDisplay();
+    
+    u8x8.setCursor(0,1);
+    u8x8.print(lati);
+    u8x8.setCursor(0,3);
+    u8x8.print(lngt);
+    
+    Serial.print(lati);
+    Serial.print(F(","));
+    Serial.print(lngt);
+    
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+    
+    u8x8.setCursor(0,1);
+    u8x8.print("NO FIX");
+    
+    u8x8.refreshDisplay();
+  }
+
+  Serial.println();
 }
